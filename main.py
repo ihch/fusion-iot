3c6e09e1519898f421921db7c1f9e35b42541e34
nemusou <ryubi.102395@gmail.com>
2018-04-07 22:10:22 +0900

    スプレッドシートへのデータ追加をできるようにした

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import json
 from pprint import pprint
+from logging import getLogger
+from spreadsheet_tools import SpreadsheetTools
 
-from googleapiclient import discovery
+logger = getLogger(__name__)
+
+SPREADSHEET_ID = "1gnInzFHiK8aGHNUH4ZgeYqGkuVkyHvqDev0yTFsPVzk"
+SCOPES = ["https://spreadsheets.google.com/feeds"]
+KEY_FILE_PATH = "./private/fusion-iot-d4cc9aaed1d2.json"
 
 
+def main():
+    spreadsheet = SpreadsheetTools(SPREADSHEET_ID, SCOPES, KEY_FILE_PATH)
+    worksheet = spreadsheet.sheets.get_worksheet(0)
+
+    """
+    data = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+            ]
+    for i, datum in enumerate(data):
+        worksheet.append_row([i] + datum)
+    """
+
+
+if __name__ == '__main__':
+    main()
