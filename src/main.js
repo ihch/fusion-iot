81b63424129ef24797fc6470246bb6971bec2526
nemusou <ryubi.102395@gmail.com>
2018-04-08 12:53:47 +0900

    コメントアウトしていていらないものを消した

--- src/main.js
+++ src/main.js
@@ -20,13 +20,8 @@ function getSpreadsheet(callback) {
           for (var j in rows) {
             res.push(rows[j].a)
           }
-          // console.log(res)
           writeChart(data.worksheets, res)
         })
-        /*
-        console.log(data.worksheets[i])
-        console.log(data.worksheets[i].title)
-        */
       }
     })
   })
@@ -37,13 +32,10 @@ function writeChart(worksheets, data) {
   if (!("ChartNumber" in writeChart)) {
     writeChart.ChartNumber = 0
   }
+  /*
   console.log(writeChart.ChartNumber)
   console.log(worksheets[writeChart.ChartNumber])
   console.log(worksheets[writeChart.ChartNumber].title)
-  /*
-  console.log(data)
-  console.log(index)
-  console.log("myChart" + String(writeChart.ChartNumber))
   */
   var ctx = document.getElementById("myChart" + String(writeChart.ChartNumber + 1))
   var options = {
@@ -60,24 +52,6 @@ function writeChart(worksheets, data) {
         // 塗りつぶし
         fill: false,
         data: data,
-        /*
-        backgroundColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        borderColor: [
-          'rgba(255,99,132,1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        */
         borderColor: [
           'rgba(244, 99, 132)'
         ],
